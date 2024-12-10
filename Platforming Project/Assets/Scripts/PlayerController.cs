403aed9545e163e9f30a72d5f46dfd42c229228d
MrBigBrain0 <sicilisi@sheridancollege.ca>
2024-12-09 20:55:55 -0500

    Added comments to all tasks/added code

--- Platforming Project/Assets/Scripts/PlayerController.cs
+++ Platforming Project/Assets/Scripts/PlayerController.cs
@@ -43,16 +43,26 @@ public class PlayerController : MonoBehaviour
     private Vector2 velocity;
 
     //Task 1 assingment 2
+
+    //tracks the players movement
     public Vector2 currentPosition;
+    //A veribale that will determin weather the player can dash
     bool hasDashed;
+    //speed that is added to the players dash
     public float dashingForce = 25f;
 
     //Task 2 assignment 2
+
+    //the first timer that will count up when tab is held down
     public float time;
+    //this is the varrible that checks if the jump can be excicuted
     bool chargeJumping;
+    //a second verrible that will start to tick upwards when the time = 3. this will rest everthing back to narmal after 2 seconds
     public float time2;
 
     //Task 3 assignment 3
+
+    //this is to sense if the player is on the spring layer
     public LayerMask Spring;
     private bool onSpring = false;
 
@@ -111,6 +121,8 @@ public class PlayerController : MonoBehaviour
         JumpUpdate();
 
         //task 1 for dashing
+
+        // the player will dash if has dashed is false and shift is pressed
         if (Input.GetKeyDown(KeyCode.LeftShift) && hasDashed == false)
             Dash();
 
@@ -121,37 +133,41 @@ public class PlayerController : MonoBehaviour
 
         body.velocity = velocity;
 
-       
+       //when the player is on the ground they can not dash
         if (isGrounded == true)
         {
             hasDashed = false;
         }
 
+        // if the player is in the air and shift is pressed the player will dash
         if(Input.GetKeyDown(KeyCode.LeftShift) && !isGrounded)
         {
             hasDashed = true;   
 
             Debug.Log("we jumping");
         }
-
+        //calls dash
         Dash();
 
         //code for task 2 assignment 2
 
+        //when is is held down and the player is on the ground time will start gaining value
         if (Input.GetKey(KeyCode.Tab) && isGrounded)
         {
             //chargeJumping = true;
 
             time += Time.deltaTime;
         }
-
+        //calls chargeJump
         ChargeJump();
 
+        //if the charge jump is true then time2 will start gaining value
         if( chargeJumping == true)
         {
             time2 += Time.deltaTime;
         }
 
+        //once time 2 hits 3 it sets charge jump false and time2 back to 0/
         if (time2 >= 3)
         {
             chargeJumping = false;  
@@ -159,8 +175,11 @@ public class PlayerController : MonoBehaviour
         }
 
         //Task 3 assignment 2
+
+        //calls checkForSpring methode
         CheckForSpring();
 
+        //this was supposed to for to send the player up when onSpring is true.
         if (onSpring)
         {
             velocity.y += 3;
@@ -213,9 +232,11 @@ public class PlayerController : MonoBehaviour
     }
 
     // task 3 assignment 2
+
+    //this should have sent the OnSpring to true when the players overlapbox sences the Spring layer.
     private void CheckForSpring()
     {
-        onSpring = Physics2D.OverlapBox(transform.position + Vector3.up * groundCheckOffset, groundCheckSize, 0, Spring);
+        onSpring = Physics2D.OverlapBox(transform.position + Vector3.down * groundCheckOffset, groundCheckSize, 0, Spring);
     }
 
     public void OnDrawGizmos()
@@ -244,7 +265,7 @@ public class PlayerController : MonoBehaviour
     //task 1
     private void Dash()
     {
-
+        //these lines of codes will send the player hurtaling in the direction they are fasing 
         if (hasDashed && currentDirection == PlayerDirection.left)
         {
             velocity.x -= velocity.x + dashingForce;
@@ -256,6 +277,7 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Dashing right");
         }
 
+        //after the code above has run has dashed is reset.
         hasDashed = false;
 
     }
@@ -263,6 +285,9 @@ public class PlayerController : MonoBehaviour
     //task 2
     private void ChargeJump()
     {
+
+        //when time which counts up when tab is held down hits 3 it will change the initialjumpspeed to 30 so that when jump is ready the player can press space and there jump will 
+        //be much higher
         if(time >= 3f  )
         {
             chargeJumping = true;
@@ -271,7 +296,7 @@ public class PlayerController : MonoBehaviour
 
             Debug.Log("JUMP READY");
 
-
+            //when time2 is at 2 or higher is will reset the initialjumpspeed and set time back to 0.
             if (time2 >= 2)
             {
                 initialJumpSpeed = 2 * apexHeight / apexTime;
